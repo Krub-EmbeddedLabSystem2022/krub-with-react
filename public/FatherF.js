5407c71e3813d5a29815be9b25ccb073841c6d91
TheresaRoss <thutgreybear@gmail.com>
2022-05-25 15:26:21 +0700

    add js file and animation

--- public/FatherF.js
+++ public/FatherF.js
@@ -1,8 +1,6 @@
-let a =0;
+
 function Animate1(){
 const hi = document.getElementById("Box");
-a++;
-hi.innerHTML = a;
 hi.style.animation="test1 2s";
 
 
@@ -10,7 +8,6 @@ hi.style.animation="test1 2s";
 
 function Animate2(){
     const hi = document.getElementById("Box");
-  
     hi.style.animation=null;
     
 }
